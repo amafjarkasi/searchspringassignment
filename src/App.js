fddb000173fa75c30c7dc824717ecd1f6dc9a043
Amaf Jarkasi <65797881+amafjarkasi@users.noreply.github.com>
2021-07-20 13:36:30 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Col,
   Form,
   Button,
+  Badge
 } from "react-bootstrap";
 import "./App.css";
 import Topbar from "./Topbar";
@@ -13,26 +14,32 @@ import Topbar from "./Topbar";
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [getSearchBar, setSearchBar] = useState("");
-  const [getResults, setResults] = useState(false);
+  const [getResults, setResults] = useState(null);
+  const [getResultsList, setResultsList] = useState();
    
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(searchResults(getSearchBar));
+    searchResults(getSearchBar);
   }
 
 async function searchResults(searchTerm) {
   const response = await fetch('https://scmq7n.a.searchspring.io/api/search/search.json?siteId=scmq7n&resultsFormat=native&q=' + searchTerm);
   const json = await response.json();
   setResults(json);
+  setResultsList(json.pagination);
   setIsOpen(true);
   return json;
+}
+
+function displayResults() {
+  return <Badge bg="secondary">{getResultsList.totalResults}</Badge>
 }
 
   return (
     <div className="App">
       <Topbar />
       <Container className="pt-3">
-        <Form>
+        <Form className="pb-4">
           <Row className="align-items-center">
             <Col sm={3} className="my-1">
               <Form.Control id="inlineFormInputName" value={getSearchBar} placeholder="Enter product" onChange={(e) => setSearchBar(e.target.value)}/>
@@ -40,9 +47,15 @@ async function searchResults(searchTerm) {
             <Col xs="auto" className="my-1">
               <Button type="submit" onClick={handleSubmit} onKeyPress={(e) => e === 13 ? handleSubmit(e) : ""}>Search</Button>
             </Col>
+            <Col xs="auto" className="my-1">
+              <Button variant="warning" onClick={() => {
+                setSearchBar("");
+                setIsOpen(false)
+                }}>Clear</Button>
+            </Col>
           </Row>
         </Form>
-        {isOpen && getResults.pagination.totalResults}
+        {isOpen && "Product results: " + <displayResults />}
       </Container>
     </div>
   );
--- src/Topbar.js
+++ src/Topbar.js
@@ -12,8 +12,6 @@ function Topbar() {
     <Navbar.Brand href="#home">searchspring</Navbar.Brand>
     <Nav className="me-auto">
       <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
     </Nav>
     </Container>
   </Navbar>
